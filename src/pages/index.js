f1a896c8814bab014a8fe4a90e010da340736239
tatsu1109 <ts11252tt1109@gmail.com>
2021-02-26 00:00:46 +0900

    検索機能を関数化

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React, { useState }from "react"
 import styled from "@emotion/styled"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -20,9 +19,90 @@ const TagsList = styled.ol`
   list-style-type:none;
 `
 
+const CategoryTag = styled.button`
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: transparent;
+  color: #0033cc;
+  &:hover {
+    text-decoration: underline;
+    color: #002080;
+  }
+`
 
-const BlogIndex = ({ location }) => {
-  const data = useStaticQuery(graphql`
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  // const posts = data.allMarkdownRemark.nodes
+  const tags = data.allMarkdownRemark.group
+  const [posts, setPosts] = useState(data.allMarkdownRemark.nodes)
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <button onClick={() => setPosts(data.allMarkdownRemark.nodes)}>Search Reset</button>
+      <Wrapper>
+        <BlogList style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <section>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <div>
+                    カテゴリー：
+                    {post.frontmatter.tags !== null
+                      ? post.frontmatter.tags.map((tag, index) =>
+                        <CategoryTag key={index} onClick={() => setPosts(searchPost(posts, tag))}>{tag}</CategoryTag>
+                      )
+                      : <CategoryTag disabled>noTag</CategoryTag>}
+                  </div>
+                  <small>{post.frontmatter.date}</small>
+                </section>
+              </article>
+            </li>
+          )
+        })}
+        </BlogList>
+        <TagsList>
+          {
+            tags.map((tag, index) => {
+              return (
+                <li key={index}>
+                  <CategoryTag onClick={() => setPosts(searchPost(posts, tag.fieldValue))}>{tag.fieldValue}</CategoryTag>
+                  {`(${tag.totalCount})`}
+                </li>
+              )
+            })
+          }
+        </TagsList>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+const searchPost = (posts, targetTag) => {
+  return posts.filter(post => {
+    if (post.frontmatter.tags !== null) {
+      return post.frontmatter.tags.find(tag => tag === targetTag)
+    }
+  })
+}
+
+export const pageQuery = graphql`
   query{
     site {
       siteMetadata {
@@ -50,94 +130,4 @@ const BlogIndex = ({ location }) => {
           totalCount
       }
     }
-  }`)
-
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  // const posts = data.allMarkdownRemark.nodes
-  const [posts, setPosts] = useState(data.allMarkdownRemark.nodes)
-  const tags = data.allMarkdownRemark.group
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      {/* <Bio /> */}
-      <button onClick={() => setPosts(data.allMarkdownRemark.nodes)}>Search Reset</button>
-      <Wrapper>
-        <BlogList style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <section>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                  <div>
-                    {post.frontmatter.tags !== null
-                      ? post.frontmatter.tags.map(
-                        buttonTag =>
-                          <button
-                            onClick={
-                              () => setPosts(
-                                posts.filter(post => {
-                                  if (post.frontmatter.tags !== null) {
-                                    return post.frontmatter.tags.find(tag => tag === buttonTag)  
-                                  }
-                                })
-                              )
-                            }
-                          >
-                            {buttonTag}
-                          </button>
-                      )
-                      : <button>noTag</button>}
-                  </div>
-                </section>
-              </article>
-            </li>
-          )
-        })}
-        </BlogList>
-        <TagsList>
-          {
-            tags.map(tag => <li>{`${tag.fieldValue}(${tag.totalCount})`}</li>)
-          }
-        </TagsList>
-      </Wrapper>
-    </Layout>
-  )
-}
-
-export default BlogIndex
-
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `
+  }`
